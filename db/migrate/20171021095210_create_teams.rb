28eb9fefacff15a7148f2623fe993d8058350f0d
faisalalsudani <f4desiign@gmail.com>
2017-10-21 11:52:33 +0200

    not to merge

---
+++ db/migrate/20171021095210_create_teams.rb
@@ -0,0 +1,12 @@
+class CreateTeams < ActiveRecord::Migration[5.1]
+  def change
+    create_table :teams do |t|
+      t.string :name
+      t.string :logo
+      t.integer :points
+      t.references :countries, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171021094417) do
+ActiveRecord::Schema.define(version: 20171021094859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 20171021094417) do
   create_table "countries", force: :cascade do |t|
     t.string "name"
     t.string "flag"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
+    t.string "logo"
+    t.integer "points", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
