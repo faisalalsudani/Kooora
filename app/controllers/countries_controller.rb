f6d47eab8faa7746cc3db05c7923defa4e811937
faisalalsudani <f4desiign@gmail.com>
2017-10-21 12:44:53 +0200

    not to merge

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -4,6 +4,27 @@ class CountriesController < ApplicationController
     @countries = Country.all
   end
 
-  
+  def show
+    @country = Country.find(params[:id])
+  end
+
+  def new
+    @country = Country.new
+  end
+
+  def create
+    @country = Country.new(countries_params)
+    if @country.save
+      redirect_to @country, notice: "Country created!"
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def countries_params
+    params.require(:country).permit(:name, :flag)
+  end
 
 end
--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,2 +1,36 @@
 class TeamsController < ApplicationController
+
+  def index
+    @teams = Team.all
+  end
+
+  def show
+    country = Country.find(params[:countries_id])
+    @team = @country.teams.find(params[:id])
+  end
+
+  def new
+    country = Country.find(params[:countries_id])
+    @team = country.teams.build
+    @team = Team.new
+  end
+
+  def create
+    country = Country.find(params[:countries_id])
+
+    @team = country.teams.create(team_params)
+
+      if @team.save
+         redirect_to @team.country
+      else
+        redirect_to @team.country, alert: "!"
+      end
+
+  end
+
+  private
+  def team_params
+    params.require(:team).permit(:name, :logo, :points, :countries_id)
+  end
+
 end
